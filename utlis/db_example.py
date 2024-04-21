a0a9cdbebbde8c1e6329e7ced44b550c4cd4b901
10956048 <10956048@ntub.edu.tw>
2024-04-21 18:34:37 +0800

    一些db連線資訊

--- utlis/db_example.py
+++ utlis/db_example.py
@@ -1,12 +1,12 @@
 #pip install pymysql
 import pymysql
 
-DB_HOST='HOST_IP'  
-DB_NAME='HOST_DB_NAME'
-DB_USER='USER_NAME'
-DB_PASSWORD='USER_PASSWORD' 
-LINE_TOKEN='Channel Access Token'
-LINE_HANDLER="Channel Secret"
+DB_HOST='140.131.114.242'  
+DB_NAME='ntub113506'
+DB_USER='113-ntub113506'
+DB_PASSWORD='@imd113506NTUB' 
+LINE_TOKEN='vzDtwf8h7fEZRRmzj4VimopZL0+T1YKif982hzSdorxlLoebj3pj/4FwFipwinhCz87gKYDRvwvWmsU5FJ+0LOhywd+LFkFSopjeArMGhyoDtH823BhMCOUxc0WVSPIfuDwNWbLCemZtEz88kCJhSQdB04t89/1O/w1cDnyilFU='
+LINE_HANDLER="1d16422c3b78ca6b26335a808c5258b2"
 
 def get_connection():
     connection=pymysql.connect(
