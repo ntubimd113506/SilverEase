d1d75b95bb50c43b73c6e2fd96ce8df4ac48b3c4
chichitung <10956014@ntub.edu.tw>
2024-04-21 14:21:55 +0800

    db connect 建立

---
+++ utlis/db_example.py
@@ -0,0 +1,15 @@
+#pip install pymysql
+import pymysql
+
+DB_HOST='HOST_IP'  
+DB_NAME='HOST_DB_NAME'
+DB_USER='USER_NAME'
+DB_PASSWORD='USER_PASSWORD' 
+
+def get_connection():
+    connection=pymysql.connect(
+    host=DB_HOST,  
+    user=DB_USER, 
+    passwd=DB_PASSWORD, 
+    database=DB_NAME)
+    return connection
