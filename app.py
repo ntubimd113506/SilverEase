21de9a825762513330d32026a87a66096803b87f
10956048 <10956048@ntub.edu.tw>
2024-04-22 21:30:55 +0800

    test**2

--- app.py
+++ app.py
@@ -31,7 +31,9 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+def handle_message(MemID, MemName, event):
+    conn = db.get_connection()
+
     # 獲取使用者的 ID
     MemID = event.source.user_id
 
@@ -39,37 +41,21 @@ def handle_message(event):
     profile = line_bot_api.get_profile(MemID)
     MemName = profile.display_name
 
+    # 定義 SQL 指令，插入使用者資料
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO User (MemID, MemName) VALUES (%s, %s)',(MemID, MemName))
+
+    conn.commit()
+    conn.close()
+
+    # 呼叫函式將使用者資料加入資料庫
+    handle_message(MemID, MemName)
+
     # 回應使用者，包括使用者名稱
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="你好，{}！你的使用者 ID 是：{}".format(MemName, MemID))
     )
-
-def add_user_to_database(MemID, MemName, event):
-    conn = db.get_connection()
-
-    try:
-        MemID = event.source.user_id
-        profile = line_bot_api.get_profile(MemID)
-        MemName = profile.display_name
-  
-        # 定義 SQL 指令，插入使用者資料
-        cursor = conn.cursor()
-        cursor.execute('INSERT INTO User (MemID, MemName) VALUES (%s, %s)',(MemID, MemName))
-
-        conn.commit()
-        
-    finally:
-        conn.close()
-
-    # 呼叫函式將使用者資料加入資料庫
-    add_user_to_database(MemID, MemName)
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="已加入資料庫")
-    )
-
 
 '''主動訊息傳送測試
 @app.route("/sent")
