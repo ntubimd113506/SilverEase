af1d4755fa1ba8b201f371af618b5d7bd6b032b2
10956048 <10956048@ntub.edu.tw>
2024-04-22 21:37:31 +0800

    test**3

--- app.py
+++ app.py
@@ -31,9 +31,7 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(MemID, MemName, event):
-    conn = db.get_connection()
-
+def handle_message(event):
     # 獲取使用者的 ID
     MemID = event.source.user_id
 
@@ -41,21 +39,19 @@ def handle_message(MemID, MemName, event):
     profile = line_bot_api.get_profile(MemID)
     MemName = profile.display_name
 
-    # 定義 SQL 指令，插入使用者資料
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO User (MemID, MemName) VALUES (%s, %s)',(MemID, MemName))
-
-    conn.commit()
-    conn.close()
-
-    # 呼叫函式將使用者資料加入資料庫
-    handle_message(MemID, MemName)
-
     # 回應使用者，包括使用者名稱
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="你好，{}！你的使用者 ID 是：{}".format(MemName, MemID))
     )
+
+def add_user_to_database(MemID, MemName):
+    conn = db.get_connection()
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO User (MemID, MemName) VALUES (%s, %s)', (MemID, MemName))
+    conn.commit()
+    conn.close()
+
 
 '''主動訊息傳送測試
 @app.route("/sent")
