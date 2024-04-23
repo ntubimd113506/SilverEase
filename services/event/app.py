af99793da454e1e19e03b134478be498d3b85c28
YI0107 <10956022@ntub.edu.tw>
2024-04-23 21:32:01 +0800

    event_app

--- services/event/app.py
+++ services/event/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for
 import sqlite3
 from flask import Blueprint
 from datetime import date
-from utlis import db_example, common
+from utlis import db, common
 
 event_bp = Blueprint('event_bp',__name__)
 
@@ -22,14 +22,13 @@ def event_create():
         Location = request.form.get('Location')
         
         #取得資料庫連線
-        conn = db_example.get_connection()
+        conn = db.get_connection()
 
         #將資料加入
         cursor = conn.cursor()
         cursor.execute("INSERT INTO Memo (Title, DateTime, Type) VALUES (%s, %s, %s)",
                         (Title, DateTime, Type))
-        cursor.execute("INSERT INTO Event (Location) VALUES (%s)",
-                        (Location))
+        cursor.execute("INSERT INTO Event (Location) VALUES (%s)", (Location))
         conn.commit()
         conn.close()
 
@@ -42,6 +41,121 @@ def event_create():
         # 渲染失敗畫面
         return render_template('create_fail.html')
 
+#刪除表單
+@event_bp.route('/delete/form')
+def event_delete_form():
+    return render_template('/event/event_delete_form.html') 
+
+#刪除確認
+@event_bp.route('/delete/confirm', methods=['GET'])
+def event_delete_confirm():
+    #取得資料庫連線    
+    connection = db.get_connection()  
+    
+    #取得執行sql命令的cursor
+    cursor = connection.cursor()   
+    
+    #取得傳入參數, 執行sql命令並取回資料  
+    MemoID = request.values.get('MemoID').strip().upper()
+      
+    cursor.execute('SELECT * FROM Event WHERE MemoID=%s', (MemoID,))
+    data = cursor.fetchone()
+
+    #關閉連線   
+    connection.close()  
+        
+    #渲染網頁
+    if data:
+        return render_template('/event/event_delete_confirm.html', data=data) 
+    else:
+        return render_template('not_found.html')
+    
+#刪除
+@event_bp.route('/delete', methods=['POST'])
+def event_delete():
+    try:
+        #取得參數
+        MemoID = request.form.get('MemoID')
+
+        #取得資料庫連線
+        conn = db.get_connection()
+
+        #將資料從event表刪除
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM Event WHERE MemoID = %s", (MemoID,))
+        
+        conn.commit()
+        conn.close()
+
+        # 渲染成功畫面
+        return render_template('delete_success.html')
+    except Exception as e:
+        #印出錯誤原因
+        print(e)
+        
+        # 渲染失敗畫面
+        return render_template('delete_fail.html')
+    
+#更改表單
+@event_bp.route('/update/form')
+def event_update_form():
+    return render_template('/event/event_update_form.html') 
+
+#更改確認
+@event_bp.route('/update/confirm', methods=['GET'])
+def event_update_confirm():
+    #取得資料庫連線    
+    connection = db.get_connection()  
+    
+    #取得執行sql命令的cursor
+    cursor = connection.cursor()   
+    
+    #取得傳入參數, 執行sql命令並取回資料  
+    MemoID = request.values.get('MemoID').strip().upper()
+      
+    cursor.execute('SELECT * FROM Event WHERE MemoID=%s', (MemoID,))
+    data = cursor.fetchone()
+
+    #關閉連線   
+    connection.close()  
+        
+    #渲染網頁
+    if data:
+        return render_template('/event/event_update_confirm.html', data=data) 
+    else:
+        return render_template('not_found.html')
+    
+    
+#更改
+@event_bp.route('/update', methods=['POST'])
+def event_update():
+    try:
+        #取得參數
+        MemoID = request.values.get('MemoID').strip().upper()
+        Title = request.form.get('Title')
+        DateTime = request.form.get('DateTime')
+        Location = request.form.get('Location')
+
+        #取得資料庫連線
+        conn = db.get_connection()
+
+        #將資料從event表刪除
+        cursor = conn.cursor()
+        cursor.execute("UPDATE Memo SET Title=%s, DateTime=%s WHERE MemoID = %s", (Title, DateTime, MemoID))
+        cursor.execute("UPDATE Event SET Location=%sWHERE MemoID = %s", (Location, MemoID))
+        
+        conn.commit()
+        conn.close()
+
+        # 渲染成功畫面
+        return render_template('update_success.html')
+    except Exception as e:
+        #印出錯誤原因
+        print(e)
+        
+        # 渲染失敗畫面
+        return render_template('update_fail.html')
+
 #查詢表單
 @event_bp.route('/read/form')
 def event_read_form():
@@ -52,7 +166,7 @@ def event_read_form():
 @event_bp.route('/read', methods=['GET'])
 def event_read():    
     #取得資料庫連線    
-    conn = db_example.get_connection()
+    conn = db.get_connection()
     
     #取得執行sql命令的cursor
     cursor = conn.cursor()   
