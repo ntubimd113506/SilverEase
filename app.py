90d9c0ae7905be22a0f4c02896f337eab62c5bd8
10956048 <10956048@ntub.edu.tw>
2024-05-01 20:09:42 +0800

    /identity

--- app.py
+++ app.py
@@ -1,33 +1,16 @@
 import requests, json
 import pymysql
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TemplateSendMessage, ConfirmTemplate, MessageAction, TextSendMessage
+from linebot.models import *
 from utlis import db
-
 
 app = Flask(__name__)
 
 # 設定你的 Line Bot 的 Channel Access Token 
 line_bot_api = LineBotApi(db.LINE_TOKEN)
-handler = WebhookHandler(db.LINE_HANDLER, TemplateSendMessage(
-
-    alt_text='ConfirmTemplate',
-    template=ConfirmTemplate(
-            text='設定身分',
-            actions=[
-                MessageAction(
-                    label='好喔',
-                    text='好喔'
-                ),
-                MessageAction(
-                    label='好喔',
-                    text='不好喔'
-                )
-            ]
-        )
-    ))
+handler = WebhookHandler(db.LINE_HANDLER)
 
 @app.route("/")
 def index():
@@ -47,20 +30,44 @@ def callback():
 
     return 'OK'
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+
+    # 回應使用者
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="請先在設定中設定您的基本資料！")
+    )
+
+#-----登入-----
+@app.route('/identity')
+def page():
+    return render_template('identity.html', liffid = '2004699458-OR9pkZjP')
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    MemID = event.source.user_id
-
-    # 獲取使用者的資訊，包括名稱
-    profile = line_bot_api.get_profile(MemID)
-    MemName = profile.display_nam
-
-    #回應使用者
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="你好，{}！你的使用者 ID 是：{}".format(MemID, MemName))
-    )
+    mtext = event.message.text
+    if mtext == 'identity':
+        message = TemplateSendMessage(
+                alt_text='按鈕樣板',
+                # template=ButtonsTemplate(
+                #     thumbnail_image_url='https://i.imgur.com/4QfKuz1.png',  #顯示的圖片
+                #     title='購買PIZZA',  #主標題
+                #     text='請選擇：',  #副標題
+                    actions=[
+                        URITemplateAction(  #開啟網頁
+                            label='連結網頁',
+                            title='身分確認',
+                            uri='https://liff.line.me/2004699458-OR9pkZjP',
+                        ),
+                    ]
+                )
+            # )
+        
+        try:
+            line_bot_api.reply_message(event.reply_token, message)
+        except:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
 
 if __name__ == "__main__":
     app.run()
