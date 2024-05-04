e2c63f15c8deb71eea7cd1ca4e091b79ef764122
10956048 <10956048@ntub.edu.tw>
2024-05-04 22:19:50 +0800

    test

--- app.py
+++ app.py
@@ -39,10 +39,46 @@ def handle_message(event):
         TextSendMessage(text="請先在設定中設定您的基本資料！")
     )
 
-#-----登入-----
+
 @app.route('/identity')
 def page():
     return render_template('identity.html', liffid = '2004699458-OR9pkZjP')
+
+@app.route('/identity/oy')
+def identity():
+    if request.form.get('options') == 'old':
+        conn = db.get_connection()
+        cursor = conn.cursor()
+
+        #取出MainUserID
+        cursor.execute('SELECT GroupID FROM Group where GroupID = MemID')
+
+        #取出資料
+        data = cursor.fetchone()
+        print(data)
+
+        conn.commit()
+        conn.close()
+        return  render_template('old.html')
+    
+    elif request.form.get('options') == 'young':
+        #資料加入資料庫
+        conn = db.get_connection()
+        cursor = conn.cursor()
+
+        #新增長輩編號
+        cursor.execute('INSERT INTO GroupLink (SubUserID) VALUES (%s)', (subno))
+        try:
+            #取得其他參數
+            subno = request.form.get('SubUserID')
+            
+        finally:
+            print(subno)
+
+        conn.commit()
+        conn.close()
+        return  render_template('young.html')
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -51,59 +87,13 @@ def handle_message(event):
         message = TemplateSendMessage(
         alt_text='按鈕樣板',
         actions=[
-                PostbackTemplateAction(  # 按鈕回傳動作
-                    label='我是長輩',
-                    data='old'
-                ),
-                PostbackTemplateAction(  # 按鈕回傳動作
-                    label='我是年輕人',
-                    data='young',
-                    uri='https://liff.line.me/2004699458-OR9pkZjP'
-                )
-            ]
+            URITemplateAction(
+                label='連結網頁',
+                title='身分確認',
+                url='https://liff.line.me/2004699458-OR9pkZjP'
+            )
+        ]
         )
-        if event.postback.data == 'old':
-            try:
-                conn = db.get_connection()
-                cursor = conn.cursor()
-                # 獲取使用者的資訊，包括名稱
-                MemID = event.source.user_id
-                profile = line_bot_api.get_profile(MemID)
-                MemName = profile.display_name
-                cursor.execute('INSERT INTO User (MemID, MemName) VALUES (%s, %s)', (MemID, MemName))
-
-                #取出MainUserID
-                cursor.execute('SELECT GroupID FROM Group where GroupID = MemID')
-
-                #取出資料
-                data = cursor.fetchone()
-                print(data)
-
-                conn.commit()
-                conn.close()
-
-                line_bot_api.reply_message(event.MemID, TextSendMessage(text="資料已成功加入資料庫！"))
-            except Exception as e:
-                # 處理資料庫操作異常
-                print("An error occurred:", e)
-                line_bot_api.reply_message(event.MemID, TextSendMessage(text="資料加入資料庫時發生錯誤！"))
-
-        elif event.postback.data == 'young':
-                conn = db.get_connection()
-                cursor = conn.cursor()
-                # 獲取使用者的資訊，包括名稱
-                MemID = event.source.user_id
-                profile = line_bot_api.get_profile(MemID)
-                MemName = profile.display_name
-                try:
-                    #取得其他參數
-                    subno = request.form.get('SubUserID')
-                    
-                finally:
-                    print(subno)
-                cursor.execute('INSERT INTO User (MemID, MemName) VALUES (%s, %s)', (MemID, MemName))
-                #新增長輩編號
-                cursor.execute('INSERT INTO GroupLink (SubUserID) VALUES (%s)', (subno))
         
         try:
             line_bot_api.reply_message(event.reply_token, message)
