ef652b8ab1939f1385675c217de8c56a67f57e32
chichitung <10956014@ntub.edu.tw>
2024-05-27 11:20:41 +0800

    MQTT success

---
+++ IoT/MQTT/src/main.cpp
@@ -0,0 +1,54 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include "my-ca.h"
+
+const char *ssid = "77";
+const char *password = "776+chocolate";
+const char *server = "silverease.ntub.edu.tw";
+const int mqtt_port = 8883;
+const char *caCert = root_ca;
+
+WiFiClientSecure espClient;
+PubSubClient mqtt(espClient);
+
+void mqttInit() {
+  mqtt.setServer(server, mqtt_port);
+  mqtt.setCallback([](char *topic, byte *payload, unsigned int length) {
+    Serial.print("收到訊息：");
+    for (int i = 0; i < length; i++) {
+      Serial.print((char)payload[i]);
+    }
+    Serial.println();
+  });
+  mqtt.connect("ESP32Client");
+  mqtt.subscribe("myTopic");
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  espClient.setCACert(caCert);
+  Serial.println("\n\n連接Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\n連接成功");
+  mqttInit();
+  delay(5000);
+  if (mqtt.connected())
+  {
+    Serial.println("Publish message");
+    mqtt.publish("myTopic", "Hello World");
+  }
+}
+
+void loop() {
+  if (!mqtt.connected()) {
+    Serial.println("重新連線");
+    mqtt.connect("ESP32Client");
+  }
+  mqtt.loop();
+}
