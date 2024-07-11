754f4d1df01a099aea7cd2d5c1ba718b7378d04d
ntub113506 <ntubimd113506@gmail.com>
2024-07-11 16:31:39 +0800

    Add analyze_bp by YI

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -3,6 +3,7 @@ from .mqtt.app import mqtt
 from .line.app import linebot_bp, line_bot_api
 from .set.app import set_bp
 from .cam.app import cam_bp
+from .analyze.app import analyze_bp
 from .event.app import event_bp
 from .hos.app import hos_bp
 from .med.app import med_bp
