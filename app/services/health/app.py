4ed5b96a8037b7955c57abb1fde5121986bc070a
YI0107 <10956022@ntub.edu.tw>
2024-08-01 16:19:55 +0800

    Change imgs

--- app/services/health/app.py
+++ app/services/health/app.py
@@ -18,38 +18,38 @@ def health_category(category):
         "death": {
             "title": "十大死因",
             "items": [
-                {"name": "惡性腫瘤(癌症)", "image": "cancel.png"},
-                {"name": "心臟疾病", "image": "code.png"},
-                {"name": "肺炎", "image": "c.png"},
-                {"name": "腦血管疾病", "image": "d.png"},
-                {"name": "糖尿病", "image": "e.png"},
-                {"name": "嚴重特殊傳染性肺炎(COVID-19)", "image": "f.png"},
-                {"name": "高血壓性疾病", "image": "g.png"},
-                {"name": "事故傷害", "image": "h.png"},
-                {"name": "慢性下呼吸道疾病", "image": "i.png"},
-                {"name": "腎炎、腎病症候群及腎病變", "image": "j.png"},
+                {"name": "惡性腫瘤(癌症)", "image": "D1.png"},
+                {"name": "心臟疾病", "image": "D2.png"},
+                {"name": "肺炎", "image": "D3.png"},
+                {"name": "腦血管疾病", "image": "D4.png"},
+                {"name": "糖尿病", "image": "D5.png"},
+                {"name": "嚴重特殊傳染性肺炎(COVID-19)", "image": "D6.png"},
+                {"name": "高血壓性疾病", "image": "D7.png"},
+                {"name": "事故傷害", "image": "D8.png"},
+                {"name": "慢性下呼吸道疾病", "image": "D9.png"},
+                {"name": "腎炎、腎病症候群及腎病變", "image": "D10.png"},
             ]
         },
         "chronic": {
             "title": "十大慢性病",
             "items": [
-                {"name": "高血壓", "image": "1.png"},
-                {"name": "糖尿病", "image": "2.png"},
-                {"name": "高血脂", "image": "3.png"},
-                {"name": "冠心病", "image": "4.png"},
-                {"name": "腦中風", "image": "5.png"},
-                {"name": "腎臟疾病", "image": "6.png"},
-                {"name": "慢性阻塞性肺病（COPD）", "image": "7.png"},
-                {"name": "癌症", "image": "8.png"},
-                {"name": "肝病", "image": "9.png"},
-                {"name": "骨質疏鬆症", "image": "10.png"},
+                {"name": "高血壓", "image": "C1.png"},
+                {"name": "糖尿病", "image": "C2.png"},
+                {"name": "高血脂", "image": "C3.png"},
+                {"name": "冠心病", "image": "C4.png"},
+                {"name": "腦中風", "image": "C5.png"},
+                {"name": "腎臟疾病", "image": "C6.png"},
+                {"name": "慢性阻塞性肺病（COPD）", "image": "C7.png"},
+                {"name": "癌症", "image": "C8.png"},
+                {"name": "肝病", "image": "C9.png"},
+                {"name": "骨質疏鬆症", "image": "C10.png"},
             ]
         },
         "precaution": {
-            "title": "防疫注意事項",
+            "title": "防疫專區",
             "items": [
-                {"name": "手部清潔", "image": "y.png"},
-                {"name": "如何正確配戴口罩", "image": "z.png"},
+                {"name": "手部清潔", "image": "P1.png"},
+                {"name": "如何正確配戴口罩", "image": "P2.png"},
             ]
         }
     }
