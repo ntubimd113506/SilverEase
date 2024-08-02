a4ef6d2fa5c6ada88c3c16050228612be7a0710d
ntub113506 <ntubimd113506@gmail.com>
2024-08-02 18:31:06 +0800

    sos report by 77

--- app/services/sos/app.py
+++ app/services/sos/app.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, session
+from datetime import datetime
 from utils import db
 
 sos_bp = Blueprint("sos_bp", __name__)
@@ -8,11 +9,15 @@ def sos_report(SOSNo):
     session["SOSNo"] = SOSNo
     conn=db.get_connection()
     cur=conn.cursor()
+    cur.execute("SELECT SOSType,SOSPlace,SOSDes,LocatNo FROM SOS WHERE SOSNo=%s",SOSNo)
+    report=cur.fetchone()
+    cur.execute("SELECT LocationTime From Location WHERE LocatNo=%s", report[-1])
+    Time=cur.fetchone()[0].strftime("%Y.%m.%d %H:%M")
     cur.execute("SELECT * FROM SOSPlace")
     Place=cur.fetchall()
     cur.execute("SELECT * FROM SOSType")
     Type=cur.fetchall()
-    return render_template("/sos/sos_report.html",Place=Place,Type=Type)
+    return render_template("/sos/sos_report.html",Place=Place,Type=Type,report=report,Time=Time)
 
 @sos_bp.route("/sos_report",methods=["POST"])
 def sos_report_post():
